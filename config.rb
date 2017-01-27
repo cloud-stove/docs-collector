76cdd01a20ed96c4174bc88d268c7332c814c49a
Christian Inzinger <christian@inz.io>
2017-01-27 10:11:45 +0100

    Use OAuth token to fetch code for CI builds

--- config.rb
+++ config.rb
@@ -46,17 +46,30 @@ configure :build do
   config[:backend_repo_url] = "git@github.com:inz/cloud-stove.git"
   config[:frontend_repo_url] = "git@github.com:inz/cloud-stove-ui.git"
 
+  if ENV['CI'] == 'true'
+    config[:backend_repo_url] = "https://$GITHUB_TOKEN:x-oauth-basic@github.com/inz/cloud-stove.git"
+    config[:frontend_repo_url] = "https://$GITHUB_TOKEN:x-oauth-basic@github.com/inz/cloud-stove-ui.git"
+  end
+
+  def fetch_code_command(directory_name, repo_url)
+    if ENV['CI'] == 'true'
+      "mkdir -p #{directory_name} && cd #{directory_name} && git init && git pull --depth=1 #{repo_url}"
+    else
+      "git clone --depth 1 #{repo_url} #{directory_name} && cd #{directory_name}"
+    end
+  end
+
   # Pull in backend docs
   activate :external_pipeline,
     name: :backend_docs,
-    command: "mkdir -p .tmp && cd .tmp && rm -rf backend && git clone --depth 1 #{config[:backend_repo_url]} backend && cd backend && yard --output-dir doc/backend",
+    command: "mkdir -p .tmp && cd .tmp && rm -rf backend && #{fetch_code_command(:backend, config[:backend_repo_url])} && yard --output-dir doc/backend",
     source: ".tmp/backend/doc",
     latency: 2
   
   # Pull in front end docs
   activate :external_pipeline,
     name: :frontend_docs,
-    command: "mkdir -p .tmp && cd .tmp && rm -rf frontend && git clone --depth 1 #{config[:frontend_repo_url]} frontend && cd frontend && npm install --ignore-scripts && npm run typedoc -- --out doc/frontend/",
+    command: "mkdir -p .tmp && cd .tmp && rm -rf frontend && #{fetch_code_command(:frontend, config[:frontend_repo_url])} && npm install --ignore-scripts && npm run typedoc -- --out doc/frontend/",
     source: ".tmp/frontend/doc",
     latency: 2
 end
